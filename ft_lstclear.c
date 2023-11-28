147d9feaf57b7483900550147ee8100f969f6135
Lee Hojoon <lhojoon@made-f0Br2s2.clusters.42paris.fr>
2023-11-28 16:11:18 +0100

    dd

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 14:49:53 by lhojoon           #+#    #+#             */
-/*   Updated: 2023/11/10 15:25:02 by lhojoon          ###   ########.fr       */
+/*   Updated: 2023/11/28 16:11:11 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	lp = *lst;
 	while (lp)
 	{
-		del(lp->content);
+		if (del != NULL)
+			del(lp->content);
 		lp2 = lp->next;
 		free(lp);
 		lp = lp2;
